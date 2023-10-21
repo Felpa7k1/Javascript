cd83d6e37fb74cc26fbc5c4d3f99fd687046463f
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-10-21 15:54:39 -0300

    operadores aritmeticos

--- 2_OPERADORES/js/script.js
+++ 2_OPERADORES/js/script.js
@@ -4,3 +4,13 @@ console.log(typeof 5.14);
 console.log(typeof -128);
 
 //2- Aritmética com Number
+console.log(2+5);
+// adição
+console.log(2-5);
+// subtração
+console.log(2*5);
+// multiplicação
+console.log(2/5);
+// divisão
+console.log((2 + 2 + 3)/3);
+// media
