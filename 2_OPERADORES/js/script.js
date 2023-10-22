9525d3b8445f2a833b59ae99fee81b3ef0c11b43
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-10-22 12:40:54 -0300

    special numbers

--- 2_OPERADORES/js/script.js
+++ 2_OPERADORES/js/script.js
@@ -1,16 +1,28 @@
 // 1 - Number
+
 console.log(2);
 console.log(typeof 5.14);
 console.log(typeof -128);
 
 //2- Aritmética com Number
-console.log(2+5);
+
+console.log(2 + 5);
 // adição
-console.log(2-5);
+console.log(2 - 5);
 // subtração
-console.log(2*5);
+console.log(2 * 5);
 // multiplicação
-console.log(2/5);
+console.log(2 / 5);
 // divisão
 console.log((2 + 2 + 3)/3);
 // media
+
+// 3 -Special Numbers
+
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * "asd");
+
+console.log(typeof NaN)
