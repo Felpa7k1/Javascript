50ce625dcdbe3e6f3eb2cf9a6a32ca64f4668852
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-10-24 08:28:32 -0300

    variáveis

---
+++ 3_ESTRUTURAS/js/script.js
@@ -0,0 +1,17 @@
+//1 - variáveis
+
+let nome = "Felipe"; //atribuir valor a variável e tipo de dado
+
+console.log(nome);
+
+nome = "Felipe Souza" // podemos atribuir um novo valor sem usar o let.
+
+console.log(nome) 
+
+const idade = 31 // const não podemos alterar o valor da variável.
+
+console.log(idade)
+
+console.log(typeof nome)
+console.log(typeof idade)
+
