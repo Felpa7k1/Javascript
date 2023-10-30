ac071ad1887c06173b0a6e27586cbf522a4c7541
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-10-30 16:41:32 -0300

    if

--- 3_ESTRUTURAS/js/script.js
+++ 3_ESTRUTURAS/js/script.js
@@ -1,36 +1,36 @@
 //1 - variáveis
 
-let nome = "Felipe"; //atribuir valor a variável e tipo de dado
+// let nome = "Felipe"; //atribuir valor a variável e tipo de dado
 
-console.log(nome);
 
-nome = "Felipe Souza" // podemos atribuir um novo valor sem usar o let.
 
-console.log(nome) 
+// nome = "Felipe Souza" // podemos atribuir um novo valor sem usar o let.
 
-const idade = 31 // const não podemos alterar o valor da variável.
+// console.log(nome)
 
-console.log(idade)
+// const idade = 31 // const não podemos alterar o valor da variável.
 
-console.log(typeof nome)
-console.log(typeof idade)
+// console.log(idade)
+
+// console.log(typeof nome)
+// console.log(typeof idade)
 
 //2 - mais sobre variáveis
 
-// let 2teste = "inválido" 
+// let 2teste = "inválido"
 // let @teste = "inválido"
 //variaveis fora da regra
 
-let a = 10, 
-b = 20, 
-c = 30;
+// let a = 10,
+// b = 20,
+// c = 30;
 
-console.log(a, b, c);
+// console.log(a, b, c);
 
-const nomecompleto = "Matheus Battisti";
-const nomeCompleto = "Felipe Souza";
+// const nomecompleto = "Matheus Battisti";
+// const nomeCompleto = "Felipe Souza";
 
-console.log(nomecompleto, nomeCompleto);
+// console.log(nomecompleto, nomeCompleto);
 
 // simbolos permitidos para criação de variáveis
 
@@ -51,35 +51,49 @@ let breakTeste = 123;
 
 //prompt
 
-const age =prompt("Digite a sua idade :");
+// const age =prompt("Digite a sua idade :");
 
-console.log("Você tem " + age + " anos");
+// console.log("Você tem " + age + " anos");
 
 //alert
 
-alert("Testando");
+// alert("Testando");
 
-const z = 10;
+// const z = 10;
 
-alert(`O número é ${z}`);
+// alert(`O número é ${z}`);
 
 
 //math
 
-console.log(Math.max(5, 2, 3, 1 , 10));
+// console.log(Math.max(5, 2, 3, 1 , 10));
 
-console.log(Math.floor(5.5));
+// console.log(Math.floor(5.5));
 
-console.log(Math.ceil(5.14));
+// console.log(Math.ceil(5.14));
 
 console.log(Math.abs(-6));
 
-console.log(Math.round(3.4))
+console.log(Math.round(3.4));
 
-// objeto console 
+//objeto console
 
-console.log("Teste!")
+console.log("Teste!");
 
-console.error("erro!")
+console.error("erro!");
 
-console.warn("aviso!")
+// console.warn("aviso!")
+
+//if - estrutura condicional
+
+let Idade = prompt("Quantos anos você tem ?");
+if(Idade >=18){
+    console.log("Você pode tirar habilitação");
+}
+const user = "João"
+
+if (user === "João"){
+    console.log("Usuário cadastrado");
+}
+
+console.log(user === "João");
