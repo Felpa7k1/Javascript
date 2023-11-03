07b527d5d4f2bc54a9a72b7fba793559b34b8adb
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-11-03 15:28:46 -0300

    return function

--- 4_FUNCOES/js/script.js
+++ 4_FUNCOES/js/script.js
@@ -29,3 +29,24 @@ idade("12");
 
 
 // 2 - Retornando valores
+
+const a = 10
+const b = 20
+const c = 30
+const d = 40
+
+function soma (n1,n2){
+    return n1 + n2;
+}
+
+const resultado = soma(a,b);
+
+console.log(resultado);
+
+console.log(soma(a , d));
+
+function sub(n1,n2){
+    return n1 - n2;
+}
+
+console.log(sub(a,d));
