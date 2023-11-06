a2a622a297612208f94a2ed13cade5098ffd0c5b
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-11-06 12:37:37 -0300

    escopo aninhado

--- 4_FUNCOES/js/script.js
+++ 4_FUNCOES/js/script.js
@@ -70,3 +70,31 @@ console.log(`Y fora da função é : ${y}`);
 
 
 testandoEscopo();
+
+
+// nested scopes
+
+let m = 10;
+
+function escopoAninhado(){
+    let m = 20;
+
+    if(true){
+        let m = 30;
+    
+    
+    if(true){
+        let m = 40;
+        console.log(m);
+    }
+        console.log(m);
+
+    }
+
+    console.log(m);
+
+}
+
+escopoAninhado();
+
+console.log(m);
