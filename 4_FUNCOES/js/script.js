1ebbe4e0daa623b515a4cef34ec5b3e9a69e51a0
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-11-08 19:42:38 -0300

    finalizando função

--- 4_FUNCOES/js/script.js
+++ 4_FUNCOES/js/script.js
@@ -268,3 +268,18 @@ console.log(c2(10));
  }
 
  untilTen(100, 7)
+
+
+ function factorial (x){
+    if(x === 0){
+        return 1
+    } else{
+        return x * factorial(x - 1)
+    }
+ }
+
+ const num = 6
+
+ const result = factorial (num)
+
+ console.log(`O fatorial do número ${num} é ${result}`)
