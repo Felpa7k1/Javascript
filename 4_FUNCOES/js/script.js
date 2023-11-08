440874424bf37b47f67a7865274f0bc30a13326c
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-11-08 19:38:51 -0300

    recursion

--- 4_FUNCOES/js/script.js
+++ 4_FUNCOES/js/script.js
@@ -252,3 +252,19 @@ const c2 = multiplicationClosure(10);
 
 console.log(c1(5));
 console.log(c2(10));
+
+// recursão
+
+ const untilTen = (n,m) =>{
+    if (n < 10){
+        console.log("A função parou de executar!")
+    } else{
+        const x = n - m 
+        console.log(x)
+
+        untilTen(x,m)
+    }
+
+ }
+
+ untilTen(100, 7)
