eb727c136a7ba650e9732db9a2ed53e49cbfcb4c
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-11-08 10:52:35 -0300

    repeat

--- 4_FUNCOES/js/script.js
+++ 4_FUNCOES/js/script.js
@@ -190,3 +190,22 @@ const greeting = (name) =>{
 
 greeting();
 greeting("Felipe");
+
+// valor default
+
+const customGreeting = (name,greet = " Olá ") => {
+    return `${greet}, ${name}!`
+}
+
+console.log(customGreeting("Matheus"));
+console.log(customGreeting("João", "Bom dia"));
+
+
+const repeatText = (text, repeat = 2) => {
+    for(let i = 0; i < repeat; i++){
+        console.log(text);
+    }
+};
+
+repeatText("Testando");
+repeatText("Agora repete 5 vezes", 5);
