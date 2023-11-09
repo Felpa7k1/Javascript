ad77824a40c4255f8bc4bb39686a5f884528e947
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-11-09 14:40:04 -0300

    propriedades

--- 5_ ARRAYS E OBJETOS/js/script.js
+++ 5_ ARRAYS E OBJETOS/js/script.js
@@ -4,3 +4,27 @@ const lista = [1 ,2 ,3 ,4 ,5]
 console.log(lista)
 
 console.log(typeof lista)
+
+const itens = ["Matheus", true, 2, 4.5, []]
+
+console.log(itens);
+
+//mais sobre arrays
+
+const arr = ["a", "b", "c", "d", "e"]
+
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[83]);
+
+
+// propriedades
+
+const numbers = [5, 3, 4]
+
+console.log(numbers.length);
+
+console.log(numbers["length"]);
+
+const myName = "Matheus";
+console.log(myName.length);
