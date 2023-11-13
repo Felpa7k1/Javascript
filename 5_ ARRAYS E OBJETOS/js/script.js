e5108468a870de478223ad8a5647a2f9318b3bf5
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-11-13 16:22:50 -0300

    mutability

--- 5_ ARRAYS E OBJETOS/js/script.js
+++ 5_ ARRAYS E OBJETOS/js/script.js
@@ -107,3 +107,24 @@ console.log(Object.keys(obj2));
 console.log(Object.keys(car));
 
 console.log(Object.entries(car));
+
+// mutability
+
+const a = {
+    name :"Matheus"
+}
+
+const b = a 
+
+console.log(a);
+console.log(b);
+
+console.log(a === b)
+
+a.age = 31
+console.log(a)
+console.log(b)
+
+delete b.age
+console.log(a);
+console.log(b);
