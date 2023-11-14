404f650835772620b4450c6357fdb398ec977422
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-11-14 17:01:41 -0300

    slice

--- 5_ ARRAYS E OBJETOS/js/script.js
+++ 5_ ARRAYS E OBJETOS/js/script.js
@@ -188,3 +188,19 @@ console.log(myElements[2])
 console.log(myElements[4])
 
 
+// slice
+
+const testeSlice = ["a", "b", "c", "d", "e", "f"]
+
+const subArray = testeSlice.slice(2,4 + 1)
+
+console.log(testeSlice)
+console.log(subArray)
+
+const subArray2 = testeSlice.slice(10,20)
+
+console.log(subArray2)
+
+const subArray3 = testeSlice.slice(2);
+
+console.log(subArray3)
