81e7e918fb7b29fd19ca1d6977a450c24095c5d6
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-11-20 16:43:17 -0300

    aprofundando em métodos

--- 6_OOP/js/script.js
+++ 6_OOP/js/script.js
@@ -10,3 +10,25 @@ const animal = {
 console.log(animal.nome)
 
 animal.latir();
+
+//2 - avançando em metódos
+
+const pessoa = {
+    nome : "Matheus",
+
+    getNome : function() {
+        return this.nome;
+    },
+
+    setNome : function (novoNome){
+        this.nome = novoNome;
+    }
+}
+
+console.log(pessoa.nome);
+
+console.log(pessoa.getNome());
+
+pessoa.setNome("Joaquim");
+
+console.log(pessoa.getNome());
