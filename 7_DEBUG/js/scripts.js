6657f5eba949e6e791a846065d8b06a442f8ae15
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-12-01 20:23:21 -0300

    debug

---
+++ 7_DEBUG/js/scripts.js
@@ -0,0 +1,20 @@
+//1 -strict
+
+"use strict";
+
+//opa = "teste";
+
+const opa = "Teste"
+
+//const undefined = 19;
+
+//delete[].length;
+
+let lista = {
+    Nome: "Felipe",
+    Profissao :"Programador"
+}
+
+console.log(lista)
+
+console.log(lista.Profissao)
