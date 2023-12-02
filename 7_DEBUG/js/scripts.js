834ba094f39c3d4380d5222903efd44ebfe67967
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-12-02 11:27:28 -0300

    metodo log

--- 7_DEBUG/js/scripts.js
+++ 7_DEBUG/js/scripts.js
@@ -10,11 +10,46 @@ const opa = "Teste"
 
 //delete[].length;
 
-let lista = {
+let ob = {
     Nome: "Felipe",
     Profissao :"Programador"
 }
 
-console.log(lista)
+console.log(ob);
 
-console.log(lista.Profissao)
+console.log(ob.Profissao);
+
+console.log(typeof ob)
+
+let lista = [
+    "Felipe",
+    "Souza",
+    3,
+    4
+]
+
+
+console.log(lista.indexOf("Souza"));
+
+
+// metodo log para debug
+
+let a = 1;
+let b = 2;
+
+if(a == 1){
+    a = b + 2
+}
+
+console.log(a);
+
+for (let i = 0; i < b; i++){
+    a = a + 2;
+    console.log(a)
+}
+
+if(a > 5){
+    a = 25;
+}
+
+console.log(a);
